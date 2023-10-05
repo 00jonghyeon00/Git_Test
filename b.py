2889eef3e5680fa8ed3958dcd0946f29370d8c93
jonghyeon <flawhdgus2@gmail.com>
2023-10-05 15:55:29 +0900

    TestTest

---
+++ b.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+# 데이터 생성
+x = np.random.randn(100, 10)
+
+# 데이터 전처리
+x_norm = x / np.sqrt(np.sum(x ** 2, axis=1, keepdims=True))
+
+# 주성분 추출
+pca = PCA(n_components=2)
+x_pca = pca.fit_transform(x_norm)
+
+# 데이터 축소
+x_red = x_pca[:, :2]
+
+# 데이터 시각화
+plt.scatter(x_red[:, 0], x_red[:, 1])
+plt.show()
+
+
+print("hi")
+print("bye")
---
+++ c.py
@@ -0,0 +1,17 @@
+
+
+
+KB캐피탈 = ['응 야근', '응 아직 더해']
+야근 = True
+
+
+if '끝' in KB캐피탈:
+    print("퇴근을 하지마")
+elif 'a':
+    print("응 너 야근")
+else:
+    print("응 너 야근")
+
+
+print("hi")
+print("bye")
